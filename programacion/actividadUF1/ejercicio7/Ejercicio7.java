19dfd4f352a267b6bb3261719375088087944dea
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-09-24 20:13:53 +0200

    añadido ejercicio 7

---
+++ programacion/actividadUF1/ejercicio7/Ejercicio7.java
@@ -0,0 +1,42 @@
+//TODO verificador de input y while.
+import java.util.*;
+
+public class Ejercicio7 {
+
+    public static void main(String[] args){
+
+        System.out.println("Bienvenido al dibujador de rectangulos.\nIntroduce el ancho seguido de Enter y despues la altura seguido de Enter.\nPara calcular un cuadrado introducce un lado y dale a Enter dos veces.");
+
+        Scanner in = new Scanner(System.in).useDelimiter("\\n");
+        String userInput = in.next();
+        int anchura = Integer.parseInt(userInput);
+        userInput = in.next();
+        int altura;
+        
+        if (userInput.isEmpty()) {
+
+            altura = anchura;
+
+        }
+
+        else {
+
+            altura = Integer.parseInt(userInput);
+
+        }
+
+        for (int i = 0; i < altura; i++) {
+
+            for (int j = 0; j < anchura; j++) {
+
+                System.out.print("  *");
+
+            }
+            
+            System.out.println();
+
+        }
+
+    }
+
+}
