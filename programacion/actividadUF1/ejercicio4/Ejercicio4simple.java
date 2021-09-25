04f4014c3ab966437b2c4ef960ff64488e255b85
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-09-25 12:18:06 +0200

    añadido ejercicio 4 simple

---
+++ programacion/actividadUF1/ejercicio4/Ejercicio4simple.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Ejercicio4simple {
+
+    public static void main(String[] args) {
+
+        System.out.print("Los numeros divisibles entre dos son: ");
+
+        for (int i = 1; i <=100; i++){
+
+            if (i % 2 == 0){
+            
+                System.out.print(i + " ");
+
+            }
+
+
+
+        }
+
+        System.out.println();
+
+        System.out.print("Los numeros divisibles entre tres son: ");
+
+        for (int i = 1; i <=100; i++){
+
+            if (i % 3 == 0){
+            
+                System.out.print(i + " ");
+
+
+            }
+
+        }
+
+    }
+
+}
