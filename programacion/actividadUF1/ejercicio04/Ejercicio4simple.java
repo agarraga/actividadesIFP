3698ec0108bfacf6e195f94e7c0a14207df1c86c
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-09-27 18:50:06 +0200

    Añadir verificador a ejercicio 6

--- programacion/actividadUF1/ejercicio04/Ejercicio4simple.java
+++ programacion/actividadUF1/ejercicio04/Ejercicio4simple.java
@@ -6,31 +6,18 @@ public class Ejercicio4simple {
 
         System.out.print("Los numeros divisibles entre dos son: ");
 
-        for (int i = 1; i <=100; i++){
-
+        for (int i = 1; i <=100; i++) {
             if (i % 2 == 0){
-            
                 System.out.print(i + " ");
-
             }
-
         }
 
-        System.out.println();
-
-        System.out.print("Los numeros divisibles entre tres son: ");
+        System.out.print("\nLos numeros divisibles entre tres son: ");
 
         for (int i = 1; i <=100; i++){
-
             if (i % 3 == 0){
-            
                 System.out.print(i + " ");
-
-
             }
-
         }
-
     }
-
 }
--- programacion/actividadUF1/ejercicio06/Ejercicio6.java
+++ programacion/actividadUF1/ejercicio06/Ejercicio6.java
@@ -12,21 +12,30 @@ public class Ejercicio6 {
         while (true) {
             System.out.println("\nIntroduce un numero y te diremos si es par o impar." +
                                "\nO pulsa 0 para saber el numero total de pares y salir.");
-            userInput = in.next(); //TODO implementar un try catch con nextINt()
+            userInput = in.next();
+           if (!userInput.matches("\\d+")) { /* TODO Asegurarse que userInput solo pueda ser un numero y no varios */
+             /*
+             * Estoy intentando hacerlo con try catch o con hasNextInt()
+             * pero no lo consigo por ahora :(
+             * Más que nada por variar que siempre utilizo el if userInput.matches(regex)
+             */
+                System.out.println("El valor introducido no es válido. " +
+                        "Por favor, introduzca un numero entero.");
+                continue;
+            }
+
             int numero = Integer.parseInt(userInput);
 
-            if (numero == 0){
-                System.out.println("\nEl total numero de pares a sido: " + totalPares + 
+            if (numero == 0) {
+                System.out.println("\nEl total numero de pares a sido: " + totalPares +
                                    " (incluyendo el cero, que tambien es par!).");
                 break;
-            }
-            else if (numero % 2 == 0) {
+            } else if (numero % 2 == 0) {
                 System.out.println("Es par!");
                 totalPares++;
-            }
-            else {
+            } else {
                 System.out.println("Es impar!");
-            } 
+            }
         }
     }
 }
