da6f15600d5a5edea61e318b45f7c77810fb42b7
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-09-30 16:31:12 +0200

    qol en ejercicios 1 y 2 .java

--- programacion/actividadUF1/ejercicio01/Ejercicio1.java
+++ programacion/actividadUF1/ejercicio01/Ejercicio1.java
@@ -8,47 +8,36 @@ public class Ejercicio1 {
         String mensaje;
         String userInput;
         Scanner in = new Scanner(System.in);
-
         System.out.println("Bienido al conversor de notas. " +
                 "Introdude un valor numérico y te diremos que nota has sacado, " +
                 "o usa \"x\" para salir:");
 
         while (true) {
-
             userInput = in.next();
 
             if (userInput.toLowerCase().equals("x")) {
                 break;
             }
-
             if (!userInput.matches("\\d+")) {
                 System.out.println(notValidInput);
                 continue;
             }
-
-            int nota = Integer.parseInt(userInput); 
             
+            int nota = Integer.parseInt(userInput); 
             if (nota < 0 || nota > 100) {
                 mensaje = notValidInput;
-            }
-            else if (nota >= 90){
+            } else if (nota >= 90){
                 mensaje = "Sobresaliente";
-            }
-            else if (nota >= 70){
+            } else if (nota >= 70){
                 mensaje = "Notable";
-            }
-            else if (nota >= 60){
+            } else if (nota >= 60){
                 mensaje = "Bien";
-            }
-            else if (nota >= 50){
+            } else if (nota >= 50){
                 mensaje = "Suficiente";
-            }
-            else {
+            } else {
                 mensaje = "Insuficiente";
             }
-
             System.out.println(mensaje);
-
         }
     }
 }
--- programacion/actividadUF1/ejercicio02/Ejercicio2.java
+++ programacion/actividadUF1/ejercicio02/Ejercicio2.java
@@ -7,13 +7,11 @@ public class Ejercicio2 {
         String userInput;
         String mensaje;
         Scanner in = new Scanner(System.in);
-
         System.out.println("Bienido al conversor a la inversa de notas." +
                            "Introdude una nota y te diremos el rango de la puntuación numerica que hayas sacado," +
                            "o usa \"x\" para salir:");
 
         while (true) {
-
             userInput = in.next();
 
             if (userInput.toLowerCase().equals("x")){
@@ -24,23 +22,18 @@ public class Ejercicio2 {
                 case "insuficiente": 
                     mensaje = "Su puntuación es menor a 50.";
                     break;
-
                 case "suficiente": 
                     mensaje = "Su puntuación es de 50 a 69.";
                     break;
-
                 case "bien": 
                     mensaje = "Su puntuación es de 60 a 79.";
                     break;
-
                 case "notable": 
                     mensaje = "Su puntuación es de 70 a 89.";
                     break;
-
                 case "sobresaliente": 
                     mensaje = "Su puntuación es de 90 a 100.";
                     break;
-
                 default: 
                     mensaje = ("Por favor, introduzca una nota valida " +
                                "(insuficiente, suficiente, bien, notable o sobresaliente)");
@@ -48,7 +41,6 @@ public class Ejercicio2 {
             }
         
             System.out.println(mensaje);
-
         }
     }
 }
