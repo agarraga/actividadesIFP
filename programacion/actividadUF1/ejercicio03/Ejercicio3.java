689118015a8a0a8b105bafdf492a813585f0027c
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-09-30 16:45:31 +0200

    fix regex y add metodo calcularAreaCirculo

--- programacion/actividadUF1/ejercicio03/Ejercicio3.java
+++ programacion/actividadUF1/ejercicio03/Ejercicio3.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class Ejercicio3 {
 
+    public static Double calacularAreaCirculo(Double radio){
+        return Math.pow(radio, 2) * Math.PI;
+    }
     public static void main(String[] args){
 
         String userInput;
@@ -10,24 +13,20 @@ public class Ejercicio3 {
         System.out.println("Introduce la radio y te diremos el area del circulo, o \"x\" para salir.");
 
         while(true){
-
             userInput = in.next();
 
             if (userInput.toLowerCase().equals("x")) {
                 break;
             }
         
-            //TODO Hay un regex que no necesite &&, también que se pueda escribir ej. .3 .6559 etc, sin el 0 delante.
-            if (!userInput.matches("\\d+\\.\\d+") && !userInput.matches("\\d+")){
+            if (!userInput.matches("^\\d*\\.*\\d+$")){
                 System.out.println("Por favor, introduce un número");
                 continue;
             }
     
-            //TODO Corto, pero puede ser un metodo
             double radio = Double.parseDouble(userInput);
-            double resultado = Math.pow(radio, 2) * (Math.PI);
-
-            System.out.println("El area de un circulo de radio " + radio + " es " + resultado) ;
+            double area = calacularAreaCirculo(radio);
+            System.out.println("El area de un circulo de radio " + radio + " es " + area) ;
         }
     }
 }
