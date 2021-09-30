5e4b531c6073e0205c832dea989df1ab93b5c3d1
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-09-30 13:56:50 +0200

    mejora ejercicio04simple

--- programacion/actividadUF1/ejercicio04/Ejercicio4simple.java
+++ programacion/actividadUF1/ejercicio04/Ejercicio4simple.java
@@ -2,22 +2,28 @@ import java.util.*;
 
 public class Ejercicio4simple {
 
+    public static Integer[] calcularNumerosDivisibles(int min, int max, int numero) {
+        List<Integer> numeros = new ArrayList<Integer>();
+        for (int i = min; i <= max; i++) {
+            if (i % numero == 0){
+                numeros.add(i);
+            }
+        }
+        Integer[] output = new Integer[numeros.size()];
+        return numeros.toArray(output);
+    }
+
+    public static void imprimirNumerosDivisbles(int min, int max, int numero) {
+        System.out.println("Los numeros divisibles entre" + numero + "son:\n");
+        Integer[] numeros = calcularNumerosDivisibles(min, max, numero);
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
+    }
+
     public static void main(String[] args) {
-
-        System.out.print("Los numeros divisibles entre dos son: ");
-
-        for (int i = 1; i <=100; i++) {
-            if (i % 2 == 0){
-                System.out.print(i + " ");
-            }
-        }
-
-        System.out.print("\nLos numeros divisibles entre tres son: ");
-
-        for (int i = 1; i <=100; i++){
-            if (i % 3 == 0){
-                System.out.print(i + " ");
-            }
-        }
+        
+        imprimirNumerosDivisbles(1, 100, 2);
+        imprimirNumerosDivisbles(1, 100, 3);
     }
 }
