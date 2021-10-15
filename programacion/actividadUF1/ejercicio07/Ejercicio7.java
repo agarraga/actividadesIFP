098b055b16bc796298ac2cf3b992cf47500a3ebd
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-10-16 01:02:02 +0200

    haz ejercicio 7 modular

--- programacion/actividadUF1/ejercicio07/Ejercicio7.java
+++ programacion/actividadUF1/ejercicio07/Ejercicio7.java
@@ -5,24 +5,32 @@ public class Ejercicio7 {
 
     public static void main(String[] args){
 
-        System.out.println("Bienvenido al dibujador de rectangulos." +
-                           "\nIntroduce el ancho seguido de Enter y despues la altura seguido de Enter."+
-                           "\nPara calcular un cuadrado introducce un lado y dale a Enter dos veces.");
-        Scanner in = new Scanner(System.in).useDelimiter("\\n");
-        String userInput = in.next();
-        int anchura = Integer.parseInt(userInput);
-        userInput = in.next();
+        String userInput; 
+        int anchura;
         int altura;
-        
+        Scanner in = new Scanner(System.in).useDelimiter("\\n");
+        System.out.println("Bienvenido al dibujador de rectangulos." +
+                           "\nIntroduce el ancho seguido de Enter " +
+                           "y despues la altura seguido de Enter."+
+                           "\nPara calcular un cuadrado introducce " +
+                           "un lado y dale a Enter dos veces.");
+        userInput = in.next();
+        anchura = Integer.parseInt(userInput);
+        userInput = in.next();
         if (userInput.isEmpty())
             altura = anchura;
         else
             altura = Integer.parseInt(userInput);
+        System.out.println(hacerRectangulo(anchura, altura)); 
+    }
 
-        for (int i = 0; i < altura; i++) {
-            for (int j = 0; j < anchura; j++)
-                System.out.print("  *");
-            System.out.println();
+    public static String hacerRectangulo(int x, int y) {
+        String cuadrado = "";
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++)
+                cuadrado.concat("  *");
+            cuadrado.concat("\\n");
         }
+        return cuadrado;
     }
 }
