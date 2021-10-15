6aea8f664957b9337a5c7520ff7f1649716bd3bf
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-10-15 23:42:05 +0200

    haz ejercicion 1 y 2 mas modulares

--- programacion/actividadUF1/ejercicio01/Ejercicio1.java
+++ programacion/actividadUF1/ejercicio01/Ejercicio1.java
@@ -4,8 +4,6 @@ public class Ejercicio1 {
     
     public static void main(String[] args) {
         
-        String notValidInput = "Por favor, introduzca un numero entro 0 y 100.";
-        String mensaje;
         String userInput;
         Scanner in = new Scanner(System.in);
         System.out.println("Bienido al conversor de notas. " +
@@ -14,29 +12,38 @@ public class Ejercicio1 {
 
         while (true) {
             userInput = in.next();
-
             if (userInput.toLowerCase().equals("x"))
                 break;
             if (!userInput.matches("\\d+")) {
-                System.out.println(notValidInput);
+                notValidInput();
                 continue;
             }
-            
             int nota = Integer.parseInt(userInput); 
-            if (nota < 0 || nota > 100) {
-                mensaje = notValidInput;
-            } else if (nota >= 90){
-                mensaje = "Sobresaliente";
-            } else if (nota >= 70){
-                mensaje = "Notable";
-            } else if (nota >= 60){
-                mensaje = "Bien";
-            } else if (nota >= 50){
-                mensaje = "Suficiente";
-            } else {
-                mensaje = "Insuficiente";
-            }
-            System.out.println(mensaje);
+            System.out.println(convertirNota(nota));
         }
+    }
+
+    public static String convertirNota(int nota) {
+        String mensaje;
+        if (nota < 0 || nota > 100) {
+            notValidInput();
+            return null;
+        } else if (nota >= 90) {
+            mensaje = "Sobresaliente";
+        } else if (nota >= 70) {
+            mensaje = "Notable";
+        } else if (nota >= 60) {
+            mensaje = "Bien";
+        } else if (nota >= 50) {
+            mensaje = "Suficiente";
+        } else {
+            mensaje = "Insuficiente";
+        }
+        return mensaje;
+    }
+
+    public static void notValidInput() {
+        String notValidInput = "Por favor, introduzca un numero entro 0 y 100.";
+        System.out.println(notValidInput);
     }
 }
