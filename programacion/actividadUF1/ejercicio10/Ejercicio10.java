100df35572793bbd025507bd0bfe8694a6925005
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-10-16 01:28:24 +0200

    refactorar ejercicio 10

--- programacion/actividadUF1/ejercicio10/Ejercicio10.java
+++ programacion/actividadUF1/ejercicio10/Ejercicio10.java
@@ -2,16 +2,19 @@ import java.util.*;
 
 public class Ejercicio10 {
 
-    public static double calcularAreaCampo(double altura){
-        return (Math.PI * Math.pow((altura/2), 2) + (altura * altura));
-    }
-
     public static void main(String[] args){
 
         //TODO verificador, while
+        String userInput;
+        Double altura;
         Scanner in = new Scanner(System.in).useDelimiter("\\n");  
-        String userInput = in.next();
-        Double altura = Double.parseDouble(userInput);
+
+        userInput = in.next();
+        altura = Double.parseDouble(userInput);
         System.out.println(calcularAreaCampo(altura));
+    }
+
+    public static double calcularAreaCampo(double altura){
+        return (Math.PI * Math.pow((altura/2), 2) + (altura * altura));
     }
 }
