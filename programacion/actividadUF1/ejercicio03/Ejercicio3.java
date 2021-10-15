8ae11ee27bc8788cd4c63ec73442ac99e4e0adff
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-10-16 00:32:47 +0200

    haz ejercicio 6 modular

--- programacion/actividadUF1/ejercicio03/Ejercicio3.java
+++ programacion/actividadUF1/ejercicio03/Ejercicio3.java
@@ -2,15 +2,12 @@ import java.util.*;
 
 public class Ejercicio3 {
 
-    public static Double calacularAreaCirculo(Double radio){
-        return Math.pow(radio, 2) * Math.PI;
-    }
     public static void main(String[] args){
 
         String userInput;
         Scanner in = new Scanner(System.in);
-
-        System.out.println("Introduce la radio y te diremos el area del circulo, o \"x\" para salir.");
+        System.out.println("Introduce la radio y te diremos " +
+                           "el area del circulo, o \"x\" para salir.");
 
         while(true){
             userInput = in.next();
@@ -22,7 +19,12 @@ public class Ejercicio3 {
             }
             double radio = Double.parseDouble(userInput);
             double area = calacularAreaCirculo(radio);
-            System.out.println("El area de un circulo de radio " + radio + " es " + area) ;
+            System.out.println("El area de un circulo de radio " + 
+                               radio + " es " + area) ;
         }
+    }
+
+    public static Double calacularAreaCirculo(Double radio){
+        return Math.pow(radio, 2) * Math.PI;
     }
 }
--- programacion/actividadUF1/ejercicio06/Ejercicio6.java
+++ programacion/actividadUF1/ejercicio06/Ejercicio6.java
@@ -6,36 +6,40 @@ public class Ejercicio6 {
 
         int totalPares = 1;
         String userInput;
-
         Scanner in = new Scanner(System.in);
 
         while (true) {
-            System.out.println("\nIntroduce un numero y te diremos si es par o impar." +
-                               "\nO pulsa 0 para saber el numero total de pares y salir.");
+            System.out.println("\nIntroduce un numero y te diremos " +
+                               "si es par o impar." +
+                               "\nO pulsa 0 para saber el numero " +
+                               "total de pares y salir.");
             userInput = in.next();
             if (!userInput.matches("^-*\\d+$")) {
-             /*
-             * Estoy intentando hacerlo con try catch o con hasNextInt()
-             * pero no lo consigo por ahora :(
-             * Más que nada por variar que siempre utilizo el if userInput.matches(regex)
-             */
                 System.out.println("El valor introducido no es válido. " +
                         "Por favor, introduzca un numero entero.");
                 continue;
             }
 
             int numero = Integer.parseInt(userInput);
-
             if (numero == 0) {
                 System.out.println("\nEl total numero de pares a sido: " + totalPares +
                                    " (incluyendo el cero, que tambien es par!).");
                 break;
-            } else if (numero % 2 == 0) {
-                System.out.println("Es par!");
-                totalPares++;
-            } else {
-                System.out.println("Es impar!");
             }
+            if (esPar(numero))
+                totalPares++;
+            imprimirEsPar(numero);
         }
+    }
+
+    public static boolean esPar(int n) {
+        return n % 2 == 0;
+    }
+
+    public static void imprimirEsPar(int n) {
+        if (esPar(n))
+            System.out.println("Es par!");
+        else
+            System.out.println("Es impar!");
     }
 }
