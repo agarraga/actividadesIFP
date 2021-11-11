f5f5cc82751ebfafa646fb053575d7a27ac90011
alonsogmunoz <alonso.g.munoz@gmail.com>
2021-11-11 17:10:56 +0100

    add getsaldoDosDecimales()

--- programacion/UF3/net/alonsoBank/Cuenta.java
+++ programacion/UF3/net/alonsoBank/Cuenta.java
@@ -9,7 +9,7 @@ public class Cuenta {
     private Double saldo = 0d;
 
     public Cuenta(String nombreCliente, String numeroCuenta) {
-        this.fechaCreacion = new Date(); 
+        fechaCreacion = new Date(); 
         this.nombreCliente = nombreCliente; 
         this.numeroCuenta = numeroCuenta; 
     }
@@ -30,6 +30,19 @@ public class Cuenta {
         return saldo;
     }
 
+    public Double getSaldoDosDecimales() {
+        /* 
+         * Creo que es mejor hacer un método separado que devuelva un string
+         * para que no se pueda utilizar en operaciones matemáticas.
+         */
+        return String.format("%.2f", saldo);
+    }
+
+    /*
+     * No he puesto `setters` ni para fechaCreacion, ya que esto no tendría que
+     * cambiar, ni para sueldo, ya que este tiene sus propios métodos con 
+     * verificadores.
+     */
     public void setNombreCliente(String nombreCliente) {
         this.nombreCliente = nombreCliente;
     }
@@ -38,23 +51,27 @@ public class Cuenta {
         this.numeroCuenta = numeroCuenta;
     }
 
-    public boolean hacerIngreso(Double cantidad) {
-        if (cantidad < 0d) {
-            return false;
+    public boolean hacerIngreso(Double importe) {
+        if (importe < 0d) {
         }
-        saldo += cantidad;
+        saldo += importe;
         return true;
     }
 
-    public boolean hacerReintegro(Double cantidad) {
-        if (cantidad < 0d || this.saldo - cantidad < 0d) {
+    public boolean hacerReintegro(Double importe) {
+        if (importe < 0d || saldo - importe < 0d) {
             return false;
         }
-        saldo -= cantidad;
+        saldo -= importe;
         return true;
     }
 
     public boolean hacerTransferencia(Cuenta destino, Double importe) {
+        /* Ya que hacerReintegro tinene verificador ambos para que la cuenta 
+         * tenga suficiente saldo para retirar, y para que no se pueda
+         * introducir un numero negativo, dejo que `hacerReintegro()` verifique
+         * `importe`, y si es válido, hacer la operación.
+         */
         if (hacerReintegro(importe)) {
             destino.hacerIngreso(importe);
             return true;
