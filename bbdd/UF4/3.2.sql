12571d22e22395eb3dd44e7433aff752aef0f9a5
agarrigu <agarrigu@protonmail.com>
2022-05-10 18:51:33 +0200

    Add bbdd/UF4/3.2.sql

--- bbdd/UF4/3.2.sql
+++ bbdd/UF4/3.2.sql
@@ -1,5 +1,5 @@
--- Crear objetos
-CREATE TYPE VEHICULOS AS OBECT (
+-- Crear objetos y cuerpos
+CREATE TYPE VEHICULO AS OBECT (
     matricula CHAR(6),
     propietario VARCHAR2(80),
     marca VARCHAR2(80),
@@ -13,27 +13,12 @@ CREATE TYPE VEHICULOS AS OBECT (
     PRAGMA RESTRICT_REFERENCES(getPotenciaCV, WNDS)
     MEMBER FUNCTION getNPlazas RETURN NUMBER,
     PRAGMA RESTRICT_REFERENCES(getNPlazas, WNDS)
-    MEMBER PROCEDURE getMatricula (SELF IN OUT NOCOPY VEHICULOS),
-    MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY VEHICULOS),
+    MEMBER PROCEDURE getMatricula (SELF IN OUT NOCOPY VEHICULO),
+    MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY VEHICULO),
 )NOT FINAL;
 
-CREATE TYPE COCHE UNDER VEHICULOS(
-    color CHAR(7),
-    descapotable BOOLEAN
-);
-
-CREATE TYPE AVION UNDER VEHICULOS(
-    nMotores NUMBER,
-    maxAltitud NUMBER
-);
-
-CREATE TYPE AVION UNDER VEHICULOS(
-    bano BOOLEAN,
-    nTV NUMBER
-);
-
 CREATE OR REPLACE 
-TYPE BODY VEHICULOS AS
+TYPE BODY VEHICULO AS
     MAP MEMBER FUNCTION getPotenciaCV RETURN NUMBER AS
     BEGIN
         RETURN potenciaCV;
@@ -44,13 +29,86 @@ TYPE BODY VEHICULOS AS
         RETURN nPlazas;
     END getNPlazas;
 
-    MEMBER PROCEDURE getMatricula (SELF IN OUT NOCOPY VEHICULOS) AS
+    MEMBER PROCEDURE getMatricula (SELF IN OUT NOCOPY VEHICULO) AS
     BEGIN
-        DBMS_OUTPUT.PUT_LINE(TO_CHAR(matricula) || ' ' || matricula || ' ' || propietario || ' ' || marca || modelo);
+        DBMS_OUTPUT.PUT_LINE(matricula || ' ' || propietario || ' ' || marca  || ' ' || modelo);
     END getMatricula;
 
-    MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY VEHICULOS) AS
+    MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY VEHICULO) AS
     BEGIN
-        DBMS_OUTPUT.PUT_LINE(TO_CHAR(matricula) || ' ' || modelo || ' ' || marca || ' ' || matricula);
+        DBMS_OUTPUT.PUT_LINE(modelo || ' ' || marca || ' ' || matricula);
     END getModelo;
 END;
+
+CREATE TYPE COCHE UNDER VEHICULO(
+    color CHAR(7),
+    descapotable CHAR(4),
+    OVERRIDE MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY COCHE),
+);
+
+
+CREATE OR REPLACE 
+TYPE BODY COCHE AS
+    MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY VEHICULO) AS
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE(modelo || ' ' || marca || ' ' || matricula || ' ' || color || ' ' || descapotable);
+    END getModelo;
+END;
+
+CREATE TYPE AVION UNDER VEHICULO(
+    nMotores NUMBER,
+    maxAltitud NUMBER,
+    OVERRIDE MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY AVION)
+);
+
+CREATE OR REPLACE 
+TYPE BODY AVION AS
+    MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY VEHICULO) AS
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE(modelo || ' ' || marca || ' ' || matricula || ' ' || TO_CHAR(nMotores) || ' ' ||  TO_CHAR(maxAltitud));
+    END getModelo;
+END;
+
+CREATE TYPE AUTOBUS UNDER VEHICULO(
+    bano CHAR(4),
+    nTV NUMBER,
+    OVERRIDE MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY AUTOBUS)
+);
+
+CREATE OR REPLACE 
+TYPE BODY AUTOBUS AS
+    MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY VEHICULO) AS
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE(modelo || ' ' || marca || ' ' || matricula || ' ' || bano);
+    END getModelo;
+END;
+
+CREATE TABLE vehiculo OF VEHICULO;
+CREATE TABLE coche OF COCHE;
+CREATE TABLE avion OF AVION;
+CREATE TABLE autobus OF AUTOBUS;
+
+-- Insertar 4 registros por subclase
+INSERT INTO coche VALUES(
+    (),
+);
+
+INSERT INTO avion VALUES(
+    (),
+);
+INSERT INTO autobus VALUES(
+    (),
+);
+
+-- Realizar consultas
+SELECT * FROM vehiculo;
+SELECT * FROM coche;
+SELECT * FROM avion;
+SELECT * FROM autobus;
+
+SELECT coche.getPotenciaCV() FROM coche;
+SELECT autobus.getNPlazas() FROM autobus;
+SELECT avion.getMatricula() FROM avion;
+SELECT coche.getModelo() FROM coche;
+SELECT autobus.getModelo() FROM autobus;
+SELECT avion.getModelo() FROM avion;
