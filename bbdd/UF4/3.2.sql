3f4a031fc5425e9b2fe7482d76b0b7b6958b6abb
agarrigu <agarrigu@protonmail.com>
2022-05-10 20:54:24 +0200

    Fix bbdd/uf4/3.2.sql

--- bbdd/UF4/3.2.sql
+++ bbdd/UF4/3.2.sql
@@ -1,5 +1,5 @@
 -- Crear objetos y cuerpos
-CREATE TYPE VEHICULO AS OBECT (
+CREATE TYPE VEHICULO AS OBJECT (
     matricula CHAR(6),
     propietario VARCHAR2(80),
     marca VARCHAR2(80),
@@ -10,11 +10,11 @@ CREATE TYPE VEHICULO AS OBECT (
     fechaMatriculacion DATE,
     ruedas NUMBER,
     MAP MEMBER FUNCTION getPotenciaCV RETURN NUMBER,
-    PRAGMA RESTRICT_REFERENCES(getPotenciaCV, WNDS)
+    PRAGMA RESTRICT_REFERENCES(getPotenciaCV, WNDS),
     MEMBER FUNCTION getNPlazas RETURN NUMBER,
-    PRAGMA RESTRICT_REFERENCES(getNPlazas, WNDS)
+    PRAGMA RESTRICT_REFERENCES(getNPlazas, WNDS),
     MEMBER PROCEDURE getMatricula (SELF IN OUT NOCOPY VEHICULO),
-    MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY VEHICULO),
+    MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY VEHICULO)
 )NOT FINAL;
 
 CREATE OR REPLACE 
@@ -43,13 +43,12 @@ END;
 CREATE TYPE COCHE UNDER VEHICULO(
     color CHAR(7),
     descapotable CHAR(4),
-    OVERRIDE MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY COCHE),
+    OVERRIDING MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY COCHE),
 );
-
 
 CREATE OR REPLACE 
 TYPE BODY COCHE AS
-    MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY VEHICULO) AS
+    OVERRIDING MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY COCHE) AS
     BEGIN
         DBMS_OUTPUT.PUT_LINE(modelo || ' ' || marca || ' ' || matricula || ' ' || color || ' ' || descapotable);
     END getModelo;
@@ -58,12 +57,12 @@ END;
 CREATE TYPE AVION UNDER VEHICULO(
     nMotores NUMBER,
     maxAltitud NUMBER,
-    OVERRIDE MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY AVION)
+    OVERRIDING MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY AVION)
 );
 
 CREATE OR REPLACE 
 TYPE BODY AVION AS
-    MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY VEHICULO) AS
+    OVERRIDING MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY AVION) AS
     BEGIN
         DBMS_OUTPUT.PUT_LINE(modelo || ' ' || marca || ' ' || matricula || ' ' || TO_CHAR(nMotores) || ' ' ||  TO_CHAR(maxAltitud));
     END getModelo;
@@ -72,12 +71,12 @@ END;
 CREATE TYPE AUTOBUS UNDER VEHICULO(
     bano CHAR(4),
     nTV NUMBER,
-    OVERRIDE MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY AUTOBUS)
+    OVERRIDING MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY AUTOBUS)
 );
 
 CREATE OR REPLACE 
 TYPE BODY AUTOBUS AS
-    MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY VEHICULO) AS
+    OVERRIDING MEMBER PROCEDURE getModelo (SELF IN OUT NOCOPY AUTOBUS) AS
     BEGIN
         DBMS_OUTPUT.PUT_LINE(modelo || ' ' || marca || ' ' || matricula || ' ' || bano);
     END getModelo;
@@ -90,14 +89,23 @@ CREATE TABLE autobus OF AUTOBUS;
 
 -- Insertar 4 registros por subclase
 INSERT INTO coche VALUES(
-    (),
+    ('123abd', 'fulanita', 'seat', 'ibiza', 400,5 ,130 , '2018-02-01', 4, '#ffeeff', 'fals'),
+    ('456jkl', 'fulanito', 'renault', 'berlingo', 600,7 ,100 , '2017-01-06', 4, '#00a08f', 'fals'),
+    ('678jgh', 'fulanite', 'ferrari', 'super-rapido', 800,2 ,180 , '2010-04-30', 4, '#e466b0', 'true'),
+    ('832akg', 'fulaniti', 'fiat', 'panda', 200,4 ,60 , '1997-02-01', 4, '#8fcef0', 'fals')
 );
 
 INSERT INTO avion VALUES(
-    (),
+    ('000023', 'Capitan Hook', 'Boeing', '747', 12000, 320, 900, '1998-02-01', 6, 5, 3500),
+    ('000453', 'Capitan Farrel', 'Airbus', '343', 10000, 180, 800, '2002-10-05', 6, 4, 2500),
+    ('000892', 'Capitan Picard', 'Starship', 'Enterprise', 2000, 320, 3999999, '2400-03-01', 0, 2, 3999999),
+    ('000023', 'Capitan Haddok', 'Ship', 'The Red Unicorn', 200, 32, 40, '1786-04-03', 0, 0, 0)
 );
 INSERT INTO autobus VALUES(
-    (),
+    ('adg943', 'La pava esta', 'Magic', 'School Bus', 200, 67, 80, '1999-04-03', 10, 'true', 0)
+    ('dkfj83', 'Hagrid', 'Magic', 'Hogwarts', 1, 69, 900, '1999-05-03', 11, 'fals', 0)
+    ('sdkfj3', 'pepe', 'mercedes', '58buzus', 200, 67, 80, '1999-04-03', 10, 'true', 0)
+    ('kdj324', 'La esta', 'Magic', 'gogo Bus', 200, 67, 80, '1999-04-03', 10, 'true', 0)
 );
 
 -- Realizar consultas
