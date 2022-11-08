25a5f3d0f455d40a523a476e53f82d25087cacef
agarrigu <agarrigu@protonmail.com>
2022-11-08 22:00:34 +0100

    Better initdb.sql for MySql

--- servidor/UF3/initdb.sql
+++ servidor/UF3/initdb.sql
@@ -6,13 +6,20 @@ CREATE TABLE usuario(
 );
 
 CREATE TABLE actividad(
-    id      INT     AUTOINCREMENT   PRIMARY KEY,
+    id      INT     AUTO_INCREMENT  PRIMARY KEY,
     titulo  VARCHAR(200)    NOT NULL    UNIQUE,
     ciudad  VARCHAR(100)    NOT NULL,
-    tipo    VARCHAR(50)     NOT NULL,
+    tipo    ENUM(
+        'viaje', 
+        'musica', 
+        'cultura', 
+        'copas', 
+        'comida', 
+        'cine'
+    )   NOT NULL,
     fecha   DATE            NOT NULL,
     gratis  BOOLEAN         NOT NULL,
-    precio  VARCHAR(50),
+    precio  ENUM('Bajo', 'Medio', 'Alto'),
     usuario VARCHAR(50)     NOT NULL,
-    FOREIGN KEY(usuario) REFERENCES usuario(id)
+    FOREIGN KEY (usuario) REFERENCES usuario(id)
 );
